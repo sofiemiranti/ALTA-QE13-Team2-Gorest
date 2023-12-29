c1b5c506931993f58b1ff2c5dfd349b2b4a600e6
Sofie Miranti Khairunnisa <sofiemirantikha@gmail.com>
2023-12-29 08:07:45 +0700

    set awal

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
+//        tags = "@Project",
         plugin = {"pretty"},
         features = "classpath:features"
 )
---
+++ src/test/java/starter/gorest/GorestAPI.java
@@ -0,0 +1,5 @@
+package starter.gorest;
+
+public class GorestAPI {
+
+}
